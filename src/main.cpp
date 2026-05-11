43b65c6e444a68865a45bf284dfacd030d5b1069
St4rgazer <St4rgazer0822@gmail.com>
2026-05-11 16:29:07 +0800

    test: StrengthReduce is NOT available

--- src/main.cpp
+++ src/main.cpp
@@ -118,7 +118,7 @@ int main(int argc, char **argv) {
         pm.addPass(std::make_unique<DimArrayArgSimplify>());
         pm.addPass(std::make_unique<Mem2Reg>());
         pm.addPass(std::make_unique<RemoveRedundantPhis>());
-        pm.addPass(std::make_unique<IndVarStrengthReduce>());
+        // pm.addPass(std::make_unique<IndVarStrengthReduce>());
 		pm.addPass(std::make_unique<TailRecursionEliminate>());
 		pm.addPass(std::make_unique<DeadCodeDelete>());
         pm.addPass(std::make_unique<LocalCopyPropagation>());
