5437ef2a5541e1da106d0dd875f671bb9a69b4e2
St4rgazer <St4rgazer0822@gmail.com>
2026-05-12 21:43:54 +0800

    test : enable LoopUnroll

--- src/main.cpp
+++ src/main.cpp
@@ -118,7 +118,6 @@ int main(int argc, char **argv) {
 	PassManager pm;
 	if(optLevel >= 1){
         // pm.addPass(std::make_unique<InlineExpand>());
-        
         pm.addPass(std::make_unique<Mem2Reg>());
         pm.addPass(std::make_unique<RemoveRedundantPhis>());
         pm.addPass(std::make_unique<LoopIdiomRecognition>());
@@ -127,7 +126,6 @@ int main(int argc, char **argv) {
         pm.addPass(std::make_unique<TailRecursionEliminate>());
         pm.addPass(std::make_unique<DeadCodeDelete>());
         pm.addPass(std::make_unique<LocalCopyPropagation>());
-        pm.addPass(std::make_unique<IndVarStrengthReduce>());
         pm.addPass(std::make_unique<ConstantFold>());
         pm.addPass(std::make_unique<AlgebraSimplify>());
         pm.addPass(std::make_unique<CSE>());
