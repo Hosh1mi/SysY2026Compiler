a501331c875e1b7e4c3377dda7a311f9ba083d70
Cinnaki <2085767206@qq.com>
2026-06-09 20:33:17 +0800

    refactor(pipeline):give up moving vectorize after unroll

--- src/main.cpp
+++ src/main.cpp
@@ -175,7 +175,7 @@ static void addInterproceduralAndGlobals(PassManager &pm) {
 }
 
 static void addLoopPipeline(PassManager &pm) {
-    pm.addPass(std::make_unique<UnifyExitNodes>());
+    // pm.addPass(std::make_unique<UnifyExitNodes>());
     pm.addPass(std::make_unique<CFGSimplify>());
     pm.addPass(std::make_unique<LoopSimplify>());
     pm.addPass(std::make_unique<LICM>());
@@ -195,6 +195,7 @@ static void buildOptimizationPipeline(PassManager &pm, int optLevel) {
     addInterproceduralAndGlobals(pm);
     addLoopPipeline(pm);
     addCanonicalCleanup(pm);
+    pm.addPass(std::make_unique<UnifyExitNodes>());
     addCfgCleanup(pm);
 
     /* Only used for experiment */
